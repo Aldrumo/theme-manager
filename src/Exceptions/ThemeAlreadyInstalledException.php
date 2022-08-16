0d0de05db2b859417329b70bc7bb9a42be538855
Mike Barlow <mike@mikebarlow.co.uk>
2022-08-16 23:01:25 +0100

    Install / uninstall / activate / deactivate functionality on model

---
+++ src/Exceptions/ThemeAlreadyInstalledException.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace Aldrumo\ThemeManager\Exceptions;
+
+use Illuminate\Http\Request;
+
+class ThemeAlreadyInstalledException extends \LogicException
+{
+    //
+}
--- src/Models/Theme.php
+++ src/Models/Theme.php
@@ -2,8 +2,14 @@
 
 namespace Aldrumo\ThemeManager\Models;
 
+use Aldrumo\ThemeManager\Exceptions\CannotUninstallActiveThemeException;
+use Aldrumo\ThemeManager\Exceptions\ThemeAlreadyActiveException;
+use Aldrumo\ThemeManager\Exceptions\ThemeAlreadyInstalledException;
+use Aldrumo\ThemeManager\Exceptions\ThemeNotActiveException;
+use Aldrumo\ThemeManager\Exceptions\ThemeNotInstalledException;
 use Aldrumo\ThemeManager\Theme\ThemeBase;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\ItemNotFoundException;
 
 class Theme extends Model
 {
@@ -24,5 +30,96 @@ class Theme extends Model
     public function getThemeBase() : ThemeBase
     {
         return app('Theme:' . $this->name);
+    }
+
+    public function install(ThemeBase $theme): bool
+    {
+        $themeName = $theme->packageName('ServiceProvider');
+
+        $model = Theme::where('name', $themeName)->first();
+        if ($model !== null) {
+            throw new ThemeAlreadyInstalledException();
+        }
+
+        $model = new Theme([
+            'name'      => $themeName,
+            'is_active' => false,
+        ]);
+
+        try {
+            $model->save();
+        } catch (\Exception $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function uninstall(ThemeBase $theme): bool
+    {
+        $themeName = $theme->packageName('ServiceProvider');
+
+        $model = Theme::where('name', $themeName)->first();
+        if ($model === null) {
+            throw new ThemeNotInstalledException();
+        }
+
+        if ($model->is_active) {
+            throw new CannotUninstallActiveThemeException();
+        }
+
+        try {
+            $model->delete();
+        } catch (\Exception $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function activate(ThemeBase $theme): bool
+    {
+        $themeName = $theme->packageName('ServiceProvider');
+
+        $model = Theme::where('name', $themeName)->first();
+        if ($model === null) {
+            throw new ThemeNotInstalledException();
+        }
+
+        if ($model->is_active) {
+            throw new ThemeAlreadyActiveException();
+        }
+
+        try {
+            $model->is_active = true;
+            $model->save();
+        } catch (\Exception $e) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public function deactivate(ThemeBase $theme): bool
+    {
+        $themeName = $theme->packageName('ServiceProvider');
+
+        $model = Theme::where('name', $themeName)->first();
+        if ($model === null) {
+            throw new ThemeNotInstalledException();
+        }
+
+        if (! $model->is_active) {
+            throw new ThemeNotActiveException();
+        }
+
+        try {
+            $model->is_active = false;
+            $model->save();
+        } catch (\Exception $e) {
+            return false;
+        }
+
+        return true;
     }
 }
