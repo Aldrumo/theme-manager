a3c8ff847db3b15d25655bdf54f1047d0476c90b
Mike Barlow <mike@mikebarlow.co.uk>
2022-08-22 08:43:25 +0100

    layout

--- resources/views/livewire/theme-admin.blade.php
+++ resources/views/livewire/theme-admin.blade.php
@@ -6,9 +6,8 @@
     <table class="min-w-full">
         <thead>
         <tr>
-            <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">{{ __('Theme Name') }}</th>
-            <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">{{ __('Installed') }}</th>
-            <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">{{ __('Active Theme') }}</th>
+            <th class="w-5/6 px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">{{ __('Theme Name') }}</th>
+            <th class="w-1/6 px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">&nbsp;</th>
         </tr>
         </thead>
 
@@ -19,44 +18,38 @@
                         <div class="leading-5 font-medium text-gray-900">
                             {{ $theme->packageName() }}
                         </div>
-                        <span class="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium leading-5 bg-green-100 text-green-800 hover:bg-green-800 hover:text-green-100">>Installed</span>
-                        <span>Active</span>
-                    </td>
-                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                        @if ($theme->isInstalled())
+                            <span class="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium leading-5 bg-green-100 text-green-800 hover:bg-green-800 hover:text-green-100">
+                                {{ __('Installed') }}
+                            </span>
+                        @endif
+
                         @if ($theme->isActive())
-                            <button class="p-2 bg-red-600 rounded-md text-white font-medium
-                                tracking-wide hover:bg-red-700 disabled:opacity-50 ..." disabled>
-                                {{ __('Uninstall Theme') }}
-                            </button>
-                        @elseif ($theme->isInstalled())
-                            <button class="p-2 bg-red-600 rounded-md text-white font-medium
-                                tracking-wide hover:bg-red-700">
-                                {{ __('Uninstall Theme') }}
-                            </button>
-                        @else
-                            <button class="p-2 bg-green-600 rounded-md text-white font-medium
-                                tracking-wide hover:bg-green-700">
-                                {{ __('Install Theme') }}
-                            </button>
+                            <span class="inline-flex items-center px-2.5 py-0.5 rounded-md text-sm font-medium leading-5 bg-red-100 text-red-800 hover:bg-red-800 hover:text-red-100">
+                                {{ __('Active') }}
+                            </span>
                         @endif
                     </td>
                     <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                        @if ($theme->isInstalled())
-                            @if ($theme->isActive())
-                                <button class="p-2 bg-green-600 rounded-md text-white font-medium
-                                tracking-wide hover:bg-green-700 disabled:opacity-50 ..." disabled>
-                                    {{ __('Active Theme') }}
-                                </button>
-                            @else
-                                <button class="p-2 bg-green-600 rounded-md text-white font-medium
-                                tracking-wide hover:bg-green-700">
-                                    {{ __('Activate') }}
-                                </button>
-                            @endif
+                        @if ($theme->isActive())
+                            <button class="p-2 bg-red-600 rounded-md text-white text-sm
+                            tracking-wide hover:bg-red-700 disabled:opacity-50 ..." disabled>
+                                {{ __('Uninstall') }}
+                            </button>
+                        @elseif ($theme->isInstalled())
+                            <button class="p-2 bg-red-600 rounded-md text-white text-sm
+                            tracking-wide hover:bg-red-700">
+                                {{ __('Uninstall') }}
+                            </button>
+
+                            <button class="p-2 bg-green-600 rounded-md text-white text-sm
+                            tracking-wide hover:bg-green-700">
+                                {{ __('Activate') }}
+                            </button>
                         @else
-                            <button class="p-2 bg-green-600 rounded-md text-white font-medium
-                                tracking-wide hover:bg-green-700">
-                                {{ __('Install Theme') }}
+                            <button class="p-2 bg-green-600 rounded-md text-white text-sm
+                            tracking-wide hover:bg-green-700">
+                                {{ __('Install') }}
                             </button>
                         @endif
                     </td>
@@ -65,3 +58,4 @@
         </tbody>
     </table>
 </div>
+
